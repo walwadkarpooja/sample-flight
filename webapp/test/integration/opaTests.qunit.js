8ae3e83cf5c44ea0f5f619e92157bb4a81ced19a
Puja Walwadkar <pooja.walwadkar1@gmail.com>
2025-06-09 13:01:19 +0000

    files added

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sampleflight/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sampleflight/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
